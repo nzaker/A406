b0ca84ab6b58d9214a63cd5901eab6995b873a3a
nzaker <62609069+nzaker@users.noreply.github.com>
2020-05-11 21:19:16 -0400

    Adding if and elif for different output
    This changes directly applied in GitHub file.

--- Assignment3/fizzbuzz.py
+++ Assignment3/fizzbuzz.py
@@ -1,11 +1,23 @@
-
 def fizz_buzz(length):
-    '''
-    fizz_buzz takes in an integer, length
-    It returns a list of strings that is 'length' strings long
-    For the each entry in the returned list: 
-        - if the index is a multiple of three, the list element will be 'Fizz'
-        - if the index is a multiple of five, the list element will be 'Buzz'
-        - if the index is a multiple of both three and five, the list element will be 'FizzBuzz'
-        - otherwise, the list element will be a string of the value of the index
-    '''
+    if (length % 15) == 0:
+        return print("FizzBuzz")
+    elif (length % 5) == 0:
+        return print("Buzz")
+    elif (length % 3) == 0:
+        return print("Fizz")
+    else:
+        return print (chr(length))
+
+
+
+fizz_buzz(len(input("please enter a name: ")))
+
+'''
+fizz_buzz takes in an integer, length
+It returns a list of strings that is 'length' strings long
+For the each entry in the returned list: 
+    - if the index is a multiple of three, the list element will be 'Fizz'
+    - if the index is a multiple of five, the list element will be 'Buzz'
+    - if the index is a multiple of both three and five, the list element will be 'FizzBuzz'
+    - otherwise, the list element will be a string of the value of the index
+'''
