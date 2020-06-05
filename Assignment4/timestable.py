3d789e23d3998840b67260b170c57a07ff5e9afe
icbicket <icbicket@users.noreply.github.com>
2020-06-05 19:00:42 -0400

    Assignment 4 functions file

---
+++ Assignment4/timestable.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def times_table_lists(number):
+    '''
+    This function returns a list of lists containing the times table for the positive integer input 'number'
+    It uses Python built-in operations in the standard library to calculate the times table
+    '''
+    
+    return times_table
+    
+def times_table_numpy(number):
+    '''
+    This function returns a 2D numpy array containing the times table for the positive integer input 'number'
+    It uses numpy functions to calculate the times table.
+    '''
+    
+    return times_table
